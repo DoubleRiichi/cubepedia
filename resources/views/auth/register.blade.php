5a94273c2d3660025454fb59511958bc5bbbd4ab
jim <doubleriichi@git.fr>
2024-09-17 16:09:14 +0200

    Added new error messages components on multiple forms

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,16 +6,12 @@
     <div class="col col-md-4 mx-auto text-center border bg-white p-3">
         <h2>Register!</h2>
         <hr>
-        @if ($errors->any())
-    <div>
-        <ul>
-            @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-    <hr>
-    @endif
+        <div class="mb-4">
+        @if($errors->any())
+            <?php $all = $errors->all();?>
+            <x-errorbox :errors="$all" />
+        @endif
+        </div>
         <x-auth.registerc/>
     </div>
 </div>
